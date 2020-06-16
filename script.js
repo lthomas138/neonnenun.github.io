f0ba236d824a0c4bbeb5bf8bceabe128e99f9a58
neonnenun <66842227+neonnenun@users.noreply.github.com>
2020-06-15 22:44:23 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 $(document).ready(function(){
+	var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+		link.type = 'image/x-icon';
+		link.rel = 'shortcut icon';
+		link.href = './images/logo.png';
+		document.getElementsByTagName('head')[0].appendChild(link);
+			
 	$("#navbar-include").load("header-nav.html");
 	$("#footer-include").load("footer-nav.html");
 });
