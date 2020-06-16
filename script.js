723a1e102b15f47148064b9473124ebd7c5dd7f9
neonnenun <66842227+neonnenun@users.noreply.github.com>
2020-06-15 21:36:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
 	$("#navbar-include").load("header-nav.html");
 	$("#footer-include").load("footer-nav.html");
+	$('#nav-menu').slideToggle();
 });
 
 $('#mobile-nav').click(function(){
-	$('#nav-menu').toggle();
+	$('#nav-menu').slideToggle();
 });
